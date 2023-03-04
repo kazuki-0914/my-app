bccbd7d81f7fcbbbe5b455fb20e34b3bdd02c8ce
“kazuki-0914” <kazu0914.dev@gmail.com>
2023-03-04 19:35:45 +0900

    test add

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+// import Todo from './component/Todo'
 
-test('renders learn react link', () => {
+jest.mock('./component/Todo', () => 'div');
+test('renders learn react link', async () => {
+
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  console.log(`screen`,screen.debug());
+  // console.log(`findAllByText`,await screen.getByText());
+  // expect(screen.getByText()).toBeUndefined();
+  expect(screen.queryByTestId('Todo')).toBeNull()
+  // const component = renderer.create(<App />);
+  // expect(component.toJSON()).toMatchSnapshot();
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
 });
